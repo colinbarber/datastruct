58e2f800318f1c8e6e94353640921309dae52f4f
Colin Barber <colinbmusic@gmail.com>
2018-07-13 15:17:07 -0400

    added A2

---
+++ ASSIGNMENTS/A2/src/com/coen352/Main.java
@@ -0,0 +1,14 @@
+package com.coen352;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private static class MergSort {
+
+    }
+
+    private class
+}
